f61a4bdd98d5e58f3fb5fffd9bf72db22ee1dc40
Wealthometer <wealthhajoh87@gmail.com>
2025-09-24 02:17:31 +0100

    update

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const express = require("express");
+import express from "express"
 
 const app = express();
 
-app.listen(3000, () => console.log("Server is running on port{}"))
+app.get("/api/auth/signup", (req, res) => {
+  res.send("Signup endpoint");
+});
+
+app.get("/api/auth/login", (req, res) => {
+  res.send("Login endpoint");
+});
+
+app.get("/api/auth/logout", (req, res) => {
+  res.send("Logout endpoint");
+});
+
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
