2dcf4dad6d442b39a2318df4cfd8f05b5d7a471a
Wealthometer <wealthhajoh87@gmail.com>
2025-09-26 21:44:12 +0100

    update

--- src/server.js
+++ src/server.js
@@ -1,22 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-app.get("/api/auth/signup", (req, res) => {
-  res.send("Signup endpoint");
-});
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
-app.get("/api/auth/login", (req, res) => {
-  res.send("Login endpoint");
-});
-
-app.get("/api/auth/logout", (req, res) => {
-  res.send("Logout endpoint");
-});
-
-app.listen(PORT, () => console.log(`Server is running on PORT :` + PORT));
+app.listen(PORT, () => console.log(`Server is running on PORT :` + PORT)); 
